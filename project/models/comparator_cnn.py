19428c5469a8c656c22759f6ecb05574524bbf15
infoshoc <infoshoc@infoshoc.infoshoc>
2018-06-16 00:19:19 +0300

    t=2 has accuracy=1

--- project/models/comparator_cnn.py
+++ project/models/comparator_cnn.py
@@ -1,4 +1,5 @@
 import sys
+import copy
 
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -8,7 +9,7 @@ from keras.layers import Conv2D, MaxPooling2D, Activation, BatchNormalization, I
 from keras.optimizers import Adam
 from keras.losses import binary_crossentropy
 from keras.metrics import binary_accuracy
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, Callback
 
 from utils.image_type import ImageType
 from utils.visualizer import PlotCallback
@@ -25,7 +26,8 @@ class ComparatorCNN:
         self._width = width
         self._height = height
         self._image_type = image_type
-        self._model = ComparatorCNN._build_model(width, height)
+        self._model = ComparatorCNN._build_model(width, height,
+                                                 post_activation_bn=True)
 
     @staticmethod
     def _build_model(width, height,
@@ -34,7 +36,7 @@ class ComparatorCNN:
                      learning_rate=1e-3, learning_rate_decay=1e-6):
         def maybe_bn(is_pre_activation):
             if is_pre_activation and pre_activation_bn or post_activation_bn:
-                return [BatchNormalization]
+                return [BatchNormalization()]
             return list()
 
         def activation():
@@ -70,30 +72,57 @@ class ComparatorCNN:
         def flatten():
             return [Flatten()]
 
+        # model = Sequential(
+        #     convolution(64, input_shape=(height, width, input_depth)) +
+        #     convolution(64) +
+        #     max_pooling() +
+        #     convolution(128) +
+        #     convolution(128) +
+        #     max_pooling() +
+        #     convolution(256) +
+        #     convolution(256) +
+        #     convolution(256) +
+        #     max_pooling() +
+        #     convolution(512) +
+        #     convolution(512) +
+        #     convolution(512) +
+        #     max_pooling() +
+        #     convolution(512) +
+        #     convolution(512) +
+        #     convolution(512) +
+        #     max_pooling() +
+        #     flatten() +
+        #     fully_connected(4096) +
+        #     fully_connected(4096) +
+        #     output_layer()
+        # )
+
         model = Sequential(
-            convolution(64, input_shape=(height, width, input_depth)) +
+            convolution(8, input_shape=(height, width, input_depth)) +
+            convolution(8) +
+            max_pooling() +
+            convolution(16) +
+            convolution(16) +
+            max_pooling() +
+            convolution(32) +
+            convolution(32) +
+            convolution(32) +
+            max_pooling() +
+            convolution(64) +
+            convolution(64) +
             convolution(64) +
             max_pooling() +
             convolution(128) +
             convolution(128) +
-            max_pooling() +
-            convolution(256) +
-            convolution(256) +
-            convolution(256) +
-            max_pooling() +
-            convolution(512) +
-            convolution(512) +
-            convolution(512) +
-            max_pooling() +
-            convolution(512) +
-            convolution(512) +
-            convolution(512) +
+            convolution(128) +
             max_pooling() +
             flatten() +
-            fully_connected(4096) +
-            fully_connected(4096) +
+            fully_connected(512) +
+            fully_connected(512) +
             output_layer()
         )
+
+        model.summary()
 
         optimizer = Adam(
             lr=learning_rate,
@@ -115,12 +144,46 @@ class ComparatorCNN:
         return model
 
     def fit_generator(self, images_train: list, batch_size, epochs, images_validation:list):
+        self._fit_standardisation(images_train)
+        images_train = self.standardise(images_train)
+        images_validation = self.standardise(images_validation)
         train_generator = ComparatorCNN._generate_regular_shreds_stratified(images_train, self._width, self._height, self._t, batch_size)
         # generator = ComparatorCNN.generate_all_shreds(images)
         validation_generator = ComparatorCNN._generate_regular_shreds_stratified(images_validation, self._width, self._height, self._t, batch_size)
         validation_data = next(validation_generator)
-        print('Part of true in validation dataset is ', np.mean(validation_data[1][1]))
+        print('Part of true in validation dataset is ', np.mean(validation_data[1][1]), ' of ', validation_data[1].shape[0])
         steps_per_epoch = 2 * len(images_train) * (self._t ** 2) / batch_size
+        print('Train mean is {} std is {} number is {}. Validation mean is {} std is {} number is {}'.format(
+            ComparatorCNN._mean_of_a_list(images_train), ComparatorCNN._std_of_a_list(images_train), len(images_train),
+            ComparatorCNN._mean_of_a_list(images_validation), ComparatorCNN._std_of_a_list(images_validation), len(images_validation),))
+
+        class UpdateMonitorCallback(Callback):
+            def __init__(self, should_monitor_updates):
+                super().__init__()
+                self._should_monitor_updates = should_monitor_updates
+                self._weights_before_batch = None
+
+            def on_batch_begin(self, batch, logs=None):
+                if self._should_monitor_updates:
+                    self._weights_before_batch = self._get_weights()
+
+            def on_batch_end(self, batch, logs=None):
+                if self._should_monitor_updates:
+                    weights_after_batch = self._get_weights()
+                    update = list(map(lambda weights: np.linalg.norm(weights[1] - weights[0]) / np.linalg.norm(weights[0]),
+                                      zip(self._weights_before_batch, weights_after_batch)))
+                    print('Update magnitudes mean is {}, they (should be ~1e-3) are: {}'.format(np.mean(update), update))
+
+            def _get_weights(self):
+                weights = list()
+
+                for layer in self.model.model.layers:
+                    current_weights = layer.get_weights()
+
+                    if len(current_weights) >= 1:
+                        weights.append(current_weights[0])
+
+                return weights
 
         self._model.fit_generator(
             train_generator,
@@ -140,33 +203,65 @@ class ComparatorCNN:
                              show=True),
                 ModelCheckpoint(self._get_model_checkpoint_file_path(),
                                 monitor='val_binary_accuracy',
-                                save_best_only=True)
+                                save_best_only=True),
+                UpdateMonitorCallback(False)
 
             ],
             validation_data=validation_data
         )
 
-    def predict_is_left(self, left, right):
+    def _fit_standardisation(self, images):
+        self._mean = ComparatorCNN._mean_of_a_list(images)
+        self._std = ComparatorCNN._std_of_a_list(images)
+
+        print('Mean is {}. Std is {}'.format(self._mean, self._std))
+
+        assert not np.isclose(self._std, 0.0), 'Variance is too low'
+
+    @staticmethod
+    def _mean_of_a_list(images_list):
+        mean = np.mean(np.concatenate(list(map(np.ndarray.flatten, images_list))))
+
+        return mean
+
+    @staticmethod
+    def _std_of_a_list(images_list):
+        std = np.std(np.concatenate(list(map(np.ndarray.flatten, images_list))))
+
+        return std
+
+    def standardise(self, images):
+        images = copy.deepcopy(images)
+
+        for index, current_image in enumerate(images):
+            images[index] = (current_image - self._mean) / self._std
+
+        return images
+
+    def predict_is_left(self, left, right, standardise=True):
         tensor = ComparatorCNN._prepare_left_right_check(left, right)
 
-        return self.predict(tensor)
+        return self.predict(tensor, standardise=standardise)
 
-    def predict_is_top(self, top, bottom):
+    def predict_is_top(self, top, bottom, standardise=True):
         tensor = ComparatorCNN._prepare_top_bottom_check(top, bottom)
 
-        return self.predict(tensor)
+        return self.predict(tensor, standardise=standardise)
 
-    def predict(self, tensor):
+    def predict(self, tensor, standardise=True):
+        if standardise:
+            tensor = self.standardise(tensor)
+
         soft_prediction = self._model.predict(tensor)
         hard_prediction = np.argmax(soft_prediction, axis=-1)
 
         return hard_prediction
 
-    def evaluate(self, images: list):
+    def evaluate(self, images: list, standardise=True):
         x, y = next(ComparatorCNN._generate_regular_shreds_stratified(images, self._width, self._height, self._t))
         print('True in evaluation dataset is {} * {}', np.mean(y[1]), y.shape[0])
         y_true = np.argmax(y, axis=-1)
-        y_predicted = self.predict(x)
+        y_predicted = self.predict(x, standardise=standardise)
 
         return np.mean(y_true == y_predicted)
 
@@ -272,6 +367,20 @@ if __name__ == '__main__':
         number_of_samples = sys.maxsize
         epochs = 200
 
+    ts = list()
+
+    if '2' in sys.argv:
+        ts.append(2)
+
+    if '4' in sys.argv:
+        ts.append(4)
+
+    if '5' in sys.argv:
+        ts = [5,]
+
+    if 0 == len(ts):
+        ts = (2, 4, 5)
+
     np.random.seed(42)
 
     width = 224
@@ -279,10 +388,7 @@ if __name__ == '__main__':
     batch_size = 32
     force = True
 
-    for t in (
-            2,
-            4,
-            5):
+    for t in ts:
         for image_type in ImageType:
             print('t={}. image type is {}'.format(t, image_type.value))
 
