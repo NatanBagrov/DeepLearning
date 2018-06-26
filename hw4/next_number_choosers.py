555704e23e6f148c157574b1fb679571f9dcf9dd
NatanBagrov <natanzb@gmail.com>
2018-06-26 21:27:42 +0300

    minor name changes to avoid confusion

--- hw4/next_number_choosers.py
+++ hw4/next_number_choosers.py
@@ -9,25 +9,27 @@ def clean_irrelevant_probabilities(softmax_probabilities):
     return softmax_probabilities / np.sum(softmax_probabilities)
 
 
-def greedy_number_chooser(next_word_softmax, clean_first=True):
-    return np.argmax(clean_irrelevant_probabilities(next_word_softmax)) if clean_first else np.argmax(next_word_softmax)
+def greedy_number_chooser(next_number_softmax, clean_first=True):
+    return np.argmax(clean_irrelevant_probabilities(next_number_softmax)) if clean_first else np.argmax(
+        next_number_softmax)
 
 
 def temperature_number_chooser_generator(temperature, clean_first=True):
-    def sample(next_word_softmax):
-        next_word_softmax = clean_irrelevant_probabilities(next_word_softmax) if clean_first else next_word_softmax
-        next_word_softmax = np.asarray(next_word_softmax).astype('float64')
-        next_word_softmax = np.log(next_word_softmax) / temperature
-        exp_preds = np.exp(next_word_softmax)
-        next_word_softmax = exp_preds / np.sum(exp_preds)
-        probas = np.random.multinomial(1, next_word_softmax, 1)
+    def sample(next_number_softmax):
+        next_number_softmax = clean_irrelevant_probabilities(
+            next_number_softmax) if clean_first else next_number_softmax
+        next_number_softmax = np.asarray(next_number_softmax).astype('float64')
+        next_number_softmax = np.log(next_number_softmax) / temperature
+        exp_preds = np.exp(next_number_softmax)
+        next_number_softmax = exp_preds / np.sum(exp_preds)
+        probas = np.random.multinomial(1, next_number_softmax, 1)
         return np.argmax(probas)
 
     return sample
 
 
-def stochastic_number_chooser(next_word_softmax, clean_first=True):
-    length = next_word_softmax.shape[0]
-    probs = clean_irrelevant_probabilities(next_word_softmax) if clean_first else next_word_softmax
+def stochastic_number_chooser(next_number_softmax, clean_first=True):
+    length = next_number_softmax.shape[0]
+    probs = clean_irrelevant_probabilities(next_number_softmax) if clean_first else next_number_softmax
     indices = np.arange(length)
     return np.random.choice(indices, p=probs)
