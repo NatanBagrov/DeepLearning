6c1368dca4bd06c99a25d06698c13d53593a47f8
infoshoc <infoshoc@infoshoc.infoshoc>
2018-07-15 19:38:58 +0300

    refactoring: split solver with comparator to generic greedy lp

---
+++ project/solvers/generic_solver_with_comparator.py
@@ -0,0 +1,191 @@
+import math
+import sys
+import os
+import time
+from timeit import timeit
+from abc import ABC, abstractmethod, abstractstaticmethod
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from pulp import LpProblem, LpMaximize, LpVariable, LpInteger, lpSum, lpDot, LpStatusOptimal, LpStatus
+
+from utils.shredder import Shredder
+from utils.data_manipulations import resize_to, shred_and_resize_to
+from utils.data_provider import DataProvider
+from utils.image_type import ImageType
+from utils.visualizer import Visualizer
+from utils.decorators import deprecated
+from models.comparator_cnn import ComparatorCNN
+
+
+class GenericSolverWithComparator(ABC):
+    def __init__(self, t_to_comparator, image_type=None):
+        self._image_type = image_type
+        self._t_to_comparator = t_to_comparator
+
+    def predict(self, shreds: list) -> list:
+        t_square = len(shreds)
+        t = int(round(math.sqrt(t_square)))
+        assert t ** 2 == t_square
+        assert t in self._t_to_comparator
+        comparator = self._t_to_comparator[t]
+        shreds = resize_to(shreds, (comparator.width, comparator.height))
+        left_index_to_right_index_to_probability = \
+            GenericSolverWithComparator._get_first_index_to_second_index_to_probability(
+                shreds,
+                comparator.predict_is_left_probability)
+        top_index_to_bottom_index_to_probability = \
+            GenericSolverWithComparator._get_first_index_to_second_index_to_probability(
+                shreds,
+                comparator.predict_is_top_probability)
+
+        prediction = self._predict(
+            left_index_to_right_index_to_probability,
+            top_index_to_bottom_index_to_probability
+        )
+
+        return prediction
+
+    def evaluate(self, images: list, ts=(2, 4, 5), epochs=1):
+        index_to_accuracy = list()
+
+        for t in ts:
+            sample_index_to_shred_index_to_image = [Shredder.shred(image, t) for image in images]
+            accuracies = list()
+
+            for epoch in range(epochs):
+                for sample_index, shred_index_to_image in enumerate(sample_index_to_shred_index_to_image):
+                    permutation = np.random.permutation(t ** 2)
+                    # permutation = np.arange(t ** 2)
+                    current_accuracy = self.evaluate_image_for_permutation(shred_index_to_image,
+                                                                           permutation,
+                                                                           sample_index)
+                    accuracies.append(current_accuracy)
+
+            current_accuracy = np.average(accuracies)
+            print('For t={} and image_type={} 0-1 is {}'.format(t, self._image_type, current_accuracy))
+            index_to_accuracy.append(current_accuracy)
+
+        return np.average(index_to_accuracy)
+
+    def evaluate_image_for_permutation(self, shred_index_to_image, permutation, sample_index=None):
+        t = int(round(math.sqrt(len(permutation))))
+
+        if isinstance(shred_index_to_image, str):
+            shred_index_to_image = DataProvider.read_image(shred_index_to_image)
+
+        if np.shape(shred_index_to_image)[0] != len(permutation):
+            shred_index_to_image = Shredder.shred(shred_index_to_image, t)
+
+        shreds_permuted = shred_index_to_image[permutation]
+        permutation_predicted = self.predict(shreds_permuted)
+        current_accuracy = np.average(permutation_predicted == permutation)
+
+        if not np.isclose(current_accuracy, 1.0):
+            print('On #{} 0-1 is {}: {}!={}'.format(sample_index, current_accuracy,
+                                                    permutation, permutation_predicted))
+            visualize = True
+
+            if visualize:
+                directory_path = 'problems/{}/{}'.format(t, self._image_type)
+                os.makedirs(directory_path, exist_ok=True)
+                time_stamp = int(time.time())
+
+                Visualizer.visualize_crops(shreds_permuted[np.argsort(permutation)],
+                                           show=False,
+                                           save_path=os.path.join(directory_path, '{}-original.png'.format(time_stamp)))
+                Visualizer.visualize_crops(shreds_permuted[np.argsort(permutation_predicted)],
+                                           show=False,
+                                           save_path=os.path.join(directory_path, '{}-restored.png'.format(time_stamp)))
+                print('visualized')
+
+        return current_accuracy
+
+    @staticmethod
+    def _get_first_index_to_second_index_to_probability(images, predict_probability):
+        first_index_to_second_index_to_first_image = np.array([
+            [
+                images[first_index]
+                for second_image in range(images.shape[0])
+            ]
+            for first_index in range(images.shape[0])
+        ])
+        first_index_to_second_index_to_second_image = np.array([
+            [
+                images[second_image]
+                for second_image in range(images.shape[0])
+            ]
+            for first_index in range(images.shape[0])
+        ])
+
+        assert (images.shape[0], images.shape[0], images.shape[1], images.shape[2]) \
+               == first_index_to_second_index_to_first_image.shape
+        assert (images.shape[0], images.shape[0], images.shape[1], images.shape[2]) \
+               == first_index_to_second_index_to_second_image.shape
+
+        first_index_to_second_index_to_first_image = np.reshape(first_index_to_second_index_to_first_image,
+                                                                (images.shape[0] ** 2, images.shape[1], images.shape[2]))
+        first_index_to_second_index_to_second_image = np.reshape(first_index_to_second_index_to_second_image,
+                                                                (images.shape[0] ** 2, images.shape[1], images.shape[2]))
+
+        first_index_to_second_index_to_probability = predict_probability(first_index_to_second_index_to_first_image,
+                                                                         first_index_to_second_index_to_second_image)
+
+        assert (images.shape[0] ** 2, 2) == \
+               first_index_to_second_index_to_probability.shape
+
+        first_index_to_second_index_to_probability = first_index_to_second_index_to_probability[:, 1]
+        first_index_to_second_index_to_probability = np.reshape(first_index_to_second_index_to_probability,
+                                                                (images.shape[0], images.shape[0]))
+
+        return first_index_to_second_index_to_probability
+
+    @staticmethod
+    def _compute_objective(crop_position_in_original_image,
+                           left_index_to_right_index_to_probability,
+                           top_index_to_bottom_index_to_probability):
+        t = int(round(math.sqrt(len(crop_position_in_original_image))))
+
+        row_to_column_to_crop_index = np.empty((t, t), dtype=int)
+
+        for crop_index, crop_position in enumerate(crop_position_in_original_image):
+            row = crop_position // t
+            column = crop_position % t
+            row_to_column_to_crop_index[row][column] = crop_index
+
+        objective = 1.0
+        log_objective = 0.0
+
+        for row in range(t):
+            for column in range(t):
+                if row + 1 < t:
+                    top_index = row_to_column_to_crop_index[row][column]
+                    bottom_index = row_to_column_to_crop_index[row + 1][column]
+                    objective *= top_index_to_bottom_index_to_probability[top_index][bottom_index]
+
+                    try:
+                        log_objective += math.log(top_index_to_bottom_index_to_probability[top_index][bottom_index])
+                    except ValueError:
+                        print('Can not calculate log({})'.format(
+                            top_index_to_bottom_index_to_probability[top_index][bottom_index])
+                        )
+                        log_objective = float("-inf")
+
+                if column + 1 < t:
+                    left_index = row_to_column_to_crop_index[row][column]
+                    right_index = row_to_column_to_crop_index[row][column + 1]
+                    objective *= left_index_to_right_index_to_probability[left_index][right_index]
+                    # TODO: bug bug bug, fails when executing log!!!!
+                    try:
+                        log_objective += math.log(left_index_to_right_index_to_probability[left_index][right_index])
+                    except ValueError:
+                        print('Can not calculate log({})'.format(
+                            left_index_to_right_index_to_probability[left_index][right_index])
+                        )
+                        log_objective = float("-inf")
+
+        return objective, log_objective
+
+    @abstractmethod
+    def _predict(self, left_index_to_right_index_to_probability, top_index_to_bottom_index_to_probability):
+        raise NotImplementedError
---
+++ project/solvers/solver_greedy.py
@@ -0,0 +1,278 @@
+import math
+import sys
+import os
+import time
+from timeit import timeit
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+from utils.shredder import Shredder
+from utils.data_manipulations import resize_to, shred_and_resize_to
+from utils.data_provider import DataProvider
+from utils.image_type import ImageType
+from utils.visualizer import Visualizer
+from utils.decorators import deprecated
+from models.comparator_cnn import ComparatorCNN
+from solvers.generic_solver_with_comparator import GenericSolverWithComparator
+
+
+class SolverGreedy(GenericSolverWithComparator):
+    def __init__(self, t_to_comparator, image_type=None):
+        GenericSolverWithComparator.__init__(self, t_to_comparator, image_type)
+
+    @staticmethod
+    def _predict_greedy_iterating_on_generic_in_generic_order(
+            left_index_to_right_index_to_probability,
+            top_index_to_bottom_index_to_probability,
+            iterate_on_bottom: bool,
+            iterate_on_right: bool,
+            column_then_row: bool,
+    ):
+        t_square = left_index_to_right_index_to_probability.shape[0]
+        t = int(round(math.sqrt(t_square)))
+        best_log_objective = float("-inf")
+        best_shred_index_to_original_index = list(range(t_square))
+
+        assert t**2 == t_square
+
+        if column_then_row and iterate_on_right or not column_then_row and iterate_on_bottom:
+            first_indices = list(reversed(range(t)))
+        else:
+            first_indices = list(range(t))
+
+        if column_then_row and iterate_on_bottom or not column_then_row and iterate_on_right:
+            second_indices = list(reversed(range(t)))
+        else:
+            second_indices = list(range(t))
+
+        for first_shred_index in range(t_square):
+            current_shred_index_to_original_index = SolverGreedy._continue_predicting_in_generic_order(
+                left_index_to_right_index_to_probability,
+                top_index_to_bottom_index_to_probability,
+                column_then_row,
+                first_shred_index,
+                first_indices,
+                second_indices
+            )
+
+            current_objective, current_log_objective = SolverGreedy._compute_objective(
+                     current_shred_index_to_original_index,
+                     left_index_to_right_index_to_probability,
+                     top_index_to_bottom_index_to_probability
+            )
+
+            if current_log_objective > best_log_objective:
+                best_log_objective = current_log_objective
+                best_shred_index_to_original_index = current_shred_index_to_original_index
+
+        return best_shred_index_to_original_index
+
+    @staticmethod
+    def _continue_predicting_in_generic_order(
+            left_index_to_right_index_to_probability,
+            top_index_to_bottom_index_to_probability,
+            column_then_row: bool,
+            first_shred_index,
+            first_indices,
+            second_indices,
+    ):
+        t_square = left_index_to_right_index_to_probability.shape[0]
+        t = int(round(math.sqrt(t_square)))
+        row_to_column_to_shred_index = [[None, ] * t for row in range(t)]
+        current_shred_index_to_original_index = [None, ] * t_square
+
+        for first_index in first_indices:
+            for second_index in second_indices:
+                if column_then_row:
+                    column = first_index
+                    row = second_index
+                else:
+                    row = first_index
+                    column = second_index
+
+                if first_indices[0] == first_index and second_indices[0] == second_index:
+                    row_to_column_to_shred_index[row][column] = first_shred_index
+                    current_shred_index_to_original_index[first_shred_index] = row * t + column
+                else:
+                    best_probability = float("-inf")
+                    best_shred_index = None
+
+                    for shred_index in range(t_square):
+                        if current_shred_index_to_original_index[shred_index] is None:
+                            current_probability = 1.0
+
+                            for row_change, column_change in ((-1, 0), (+1, 0), (0, -1), (0, +1)):
+                                if 0 != row_change and 0 <= row + row_change < t \
+                                        and row_to_column_to_shred_index[row + row_change][column] is not None:
+                                    if row_change == -1:
+                                        top_index = row_to_column_to_shred_index[row + row_change][column]
+                                        bottom_index = shred_index
+                                    else:
+                                        top_index = shred_index
+                                        bottom_index = row_to_column_to_shred_index[row + row_change][column]
+
+                                    vertical_probability = \
+                                        top_index_to_bottom_index_to_probability[top_index][bottom_index]
+                                else:
+                                    vertical_probability = 1.0
+
+                                if 0 != column_change and 0 <= column + column_change < t \
+                                        and row_to_column_to_shred_index[row][column + column_change] is not None:
+                                    if column_change == -1:
+                                        left_index = row_to_column_to_shred_index[row][column + column_change]
+                                        right_index = shred_index
+                                    else:
+                                        left_index = shred_index
+                                        right_index = row_to_column_to_shred_index[row][column + column_change]
+
+                                    horizontal_probability = \
+                                        left_index_to_right_index_to_probability[left_index][right_index]
+                                else:
+                                    horizontal_probability = 1.0
+
+                                current_probability *= vertical_probability * horizontal_probability
+
+                            if current_probability > best_probability:
+                                best_probability = current_probability
+                                best_shred_index = shred_index
+
+                    row_to_column_to_shred_index[row][column] = best_shred_index
+                    current_shred_index_to_original_index[best_shred_index] = row * t + column
+
+        return current_shred_index_to_original_index
+
+    def _predict(self,
+                 left_index_to_right_index_to_probability,
+                 top_index_to_bottom_index_to_probability):
+        best_log_objective = float("-inf")
+        best_crop_position_in_original_image = None
+        best_configuration = None
+
+        for iterate_on_bottom in (False, True):
+            for iterate_on_right in (False, True):
+                for column_then_row in (False, True):
+                    current_crop_position_in_original_image = \
+                        SolverGreedy._predict_greedy_iterating_on_generic_in_generic_order(
+                        left_index_to_right_index_to_probability,
+                        top_index_to_bottom_index_to_probability,
+                        iterate_on_bottom,
+                        iterate_on_right,
+                        column_then_row
+                    )
+
+                    current_objective, current_log_objective = \
+                        SolverGreedy._compute_objective(
+                            current_crop_position_in_original_image,
+                            left_index_to_right_index_to_probability,
+                            top_index_to_bottom_index_to_probability)
+
+                    if best_crop_position_in_original_image is None or current_log_objective > best_log_objective:
+                        best_log_objective = current_log_objective
+                        best_configuration = (iterate_on_bottom, iterate_on_right, column_then_row)
+                        best_crop_position_in_original_image = current_crop_position_in_original_image
+
+        print('Using {} {} {}-wise'.format(
+            'bottom' if best_configuration[0] else 'top',
+            'right' if best_configuration[1] else 'left',
+            'column' if best_configuration[2] else 'row',
+        ))
+
+        return best_crop_position_in_original_image
+
+
+def main():
+    if 'debug' in sys.argv:
+        print('Debug')
+        number_of_samples = 20
+        epochs = 1
+    else:
+        print('Release')
+        number_of_samples = sys.maxsize
+        epochs = 5
+
+    ts = list()
+
+    if '2' in sys.argv:
+        ts.append(2)
+
+    if '4' in sys.argv:
+        ts.append(4)
+
+    if '5' in sys.argv:
+        ts = [5,]
+
+    if 0 == len(ts):
+        ts = (2, 4, 5)
+
+    image_types = list()
+
+    if 'image' in sys.argv:
+        image_types.append(ImageType.IMAGES)
+
+    if 'document' in sys.argv:
+        image_types.append(ImageType.DOCUMENTS)
+
+    if 0 == len(image_types):
+        image_types = ImageType
+
+    np.random.seed(42)
+
+    width = 224
+    height = 224
+
+    for image_type in image_types:
+        print(image_type.value)
+
+        if image_type == ImageType.IMAGES:
+            get_images = DataProvider().get_fish_images
+            mean = 100.52933494138787
+            std = 65.69793156777682
+        else:
+            get_images = DataProvider().get_docs_images
+            mean = 241.46115784237548
+            std = 49.512839464023564
+
+        images, names = get_images(num_samples=number_of_samples, return_names=True)
+
+        images_train, images_validation, names_train, names_validation = train_test_split(images, names,
+                                                                                          random_state=42)
+        t_to_comparator = {
+            t: ComparatorCNN(t, width, height, image_type, mean=mean, std=std)
+                .load_weights()
+            for t in ts
+        }
+
+        clf = SolverGreedy(t_to_comparator, image_type=image_type)
+        print('Train: ', names_train)
+        accuracy = clf.evaluate(images_train, epochs=epochs, ts=ts)
+        print('Train 0-1 accuracy on {}: {}'.format(image_type.value, accuracy))
+        print('Validation: ', names_validation)
+        accuracy = clf.evaluate(images_validation, epochs=epochs, ts=ts)
+        print('Validation 0-1 accuracy on {}: {}'.format(image_type.value, accuracy))
+
+
+def debug():
+    names_train = ['n01440764_11593.JPEG', 'n01440764_11602.JPEG', 'n01440764_4562.JPEG', 'n01440764_5148.JPEG', 'n01440764_11897.JPEG', 'n01440764_29057.JPEG', 'n01440764_22135.JPEG', 'n01440764_8003.JPEG', 'n01440764_3566.JPEG', 'n01440764_44.JPEG', 'n01440764_10910.JPEG', 'n01440764_10382.JPEG', 'n01440764_6508.JPEG', 'n01440764_10290.JPEG', 'n01440764_910.JPEG']
+    images_train, names_train = DataProvider.read_images('../images', names_train)
+    indices = [4, 5, 13]
+    images_validation = [images_train[index] for index in indices]
+    index = 4
+    t = 4
+    permutation = [2, 8, 4, 12, 0, 10, 6, 5, 7, 3, 13, 15, 11, 9, 1, 14]
+    width = 224
+    height = 224
+    image_type = ImageType.IMAGES
+
+    cmp = ComparatorCNN(t, width, height, image_type)\
+        ._fit_standardisation(images_train)\
+        .load_weights()
+
+    slv = SolverGreedy({t: cmp})
+    score = slv.evaluate_image_for_permutation(images_validation[0], permutation, sample_index=index)
+    print('done with ', score)
+
+
+if __name__ == '__main__':
+    # debug()
+    main()
